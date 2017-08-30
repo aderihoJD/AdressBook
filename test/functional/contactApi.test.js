2f8792c7bcff24d95ecd51424bcdb9180f2cb1d0
aderiho_ilya <ilya_aderiho@icloud.com>
2017-08-31 02:11:18 +0300

    Add functional DELETE test

--- test/functional/contactApi.test.js
+++ test/functional/contactApi.test.js
@@ -96,8 +96,35 @@ describe("Contact API test",()=>{
 
         })
 
+        describe("#DELETE /api/contact/:id", ()=> {
 
-})
+            it("shoud find contact and remove it", (done) => {
+                const name = shortid.generate();
+                const email = "ilya@mail.ru";
+                const phone_number = "+375-25-793-76-17";
 
+                chai
+                    .request(`http://localhost:${port}`)
+                    .post('/api/contact')
+                    .send({name, email, phone_number})
+                    .then((res) => {
+                        return chai
+                            .request(`http://localhost:${port}`)
+                            .delete(`/api/contact/${res.body._id}`)
+                    })
+                    .then((res) => {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property("_id");
+                        expect(res.body).to.have.property("name", name);
+                        expect(res.body).to.have.property("email", email);
+                        expect(res.body).to.have.property("phone_number", phone_number);
+                        done();
+                    })
+                    .catch(done);
+
+            })
+
+        })
+    })
 
 });

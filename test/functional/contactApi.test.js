d5612d83c94c91cd77ad15f1b4fbd4194e410eb4
aderiho_ilya <ilya_aderiho@icloud.com>
2017-08-31 02:08:02 +0300

    Add functional PUT test

--- test/functional/contactApi.test.js
+++ test/functional/contactApi.test.js
@@ -64,7 +64,40 @@ describe("Contact API test",()=>{
         })
 
     })
+    describe("#PUT /api/contact", ()=> {
 
+        it("shoud find contact and replace it", (done) => {
+            const name = shortid.generate();
+            const email = "ilya@mail.ru";
+            const phone_number = "+375-25-793-76-17";
+            const newName = shortid.generate();
+            const newEmail = "igar@mail.ru";
+            const newPhone_number = "+375-25-793-76-10";
+
+            chai
+                .request(`http://localhost:${port}`)
+                .post('/api/contact')
+                .send({name, email, phone_number})
+                .then((res) => {
+                    return chai
+                        .request(`http://localhost:${port}`)
+                        .put(`/api/contact`)
+                        .send({_id:res.body._id,name:newName, email:newEmail, phone_number:newPhone_number})
+                })
+                .then((res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property("_id");
+                    expect(res.body).to.have.property("name", newName);
+                    expect(res.body).to.have.property("email", newEmail);
+                    expect(res.body).to.have.property("phone_number", newPhone_number);
+                    done();
+                })
+                .catch(done);
+
+        })
+
+
+})
 
 
 });
